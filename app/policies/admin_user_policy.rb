5124926698473bd747355527f6b551bfd5d25a10
Margarita Barvinok <brita@umich.edu>
2026-01-23 15:19:54 -0500

    add policies

---
+++ app/policies/admin_user_policy.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class AdminUserPolicy  < ApplicationPolicy
+  attr_reader :user, :record
+
+  def index?
+    admin_user?
+  end
+
+  def create?
+    admin_user?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    admin_user?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    admin_user?
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
