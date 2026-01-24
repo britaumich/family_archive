19a1edf8860d5b2987d73d6af38c335f2058ab34
Margarita Barvinok <brita@umich.edu>
2026-01-23 23:41:08 -0500

    remove comment

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,16 +7,12 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # if AdminUser.find_by(email: @user.email_address).present?
-      if @user.save
-        start_new_session_for(@user)
-        redirect_to root_url, notice: t('forms.messages.Registered successfully')
-      else
-        render :new, status: :unprocessable_entity
-      end
-    # else
-    #   redirect_to new_registration_path, alert: t('forms.messages.Email address is not allowed to register')
-    # end
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to root_url, notice: t('forms.messages.Registered successfully')
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
