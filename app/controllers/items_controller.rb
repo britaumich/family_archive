c9b3c3217f7557d90a55f8173287d6db0237babf
Margarita Barvinok <brita@umich.edu>
2026-01-29 22:16:43 -0500

    add bulk_assign_tags feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -99,6 +99,94 @@ class ItemsController < ApplicationController
     end
   end
 
+  def bulk_assign_tags_form
+    authorize Item
+    
+    @tags = Tag.includes(:tag_type).order('tag_types.name ASC NULLS LAST, tags.name ASC')
+    @tags_by_type = @tags.group_by(&:tag_type)
+    @selected_tags = []
+
+    # Filter by multiple tags if specified
+    if params[:tags].present?
+      tag_ids = params[:tags].reject(&:blank?).map(&:to_i)
+      @selected_tags = Tag.where(id: tag_ids)
+
+      if tag_ids.any?
+        # AND logic: items must have ALL selected tags
+        if params[:filter_type] == 'all'
+          # Use subquery to find items with all required tags
+          item_ids = Item.joins(:tags)
+                         .where(tags: { id: tag_ids })
+                         .group('items.id')
+                         .having('COUNT(DISTINCT tags.id) = ?', tag_ids.length)
+                         .pluck('items.id')
+          @items = Item.includes(:tags).with_attached_file
+                       .where(id: item_ids)
+                       .order(created_at: :desc)
+        else
+          # OR logic (default): items must have ANY of the selected tags
+          @items = Item.includes(:tags).with_attached_file
+                       .joins(:tags)
+                       .where(tags: { id: tag_ids })
+                       .distinct
+                       .order(created_at: :desc)
+        end
+      else
+        @items = Item.includes(:tags).with_attached_file.order(created_at: :desc)
+      end
+    else
+      @items = Item.includes(:tags).with_attached_file.order(created_at: :desc)
+    end
+    
+    # Also prepare tags organized by type for assignment
+    @assignment_tags_by_type = Tag.joins(:tag_type).includes(:tag_type)
+                               .group_by(&:tag_type)
+                               .transform_keys(&:name)
+                               .sort
+    @assignment_tags_without_type = Tag.where(tag_type: nil)
+    # @assignment_tags_by_type['No Type'] = @assignment_tags_without_type if @assignment_tags_without_type.any?
+  end
+
+  def bulk_assign_tags
+    item_ids = params[:item_ids]&.reject(&:blank?)
+    tag_ids = params[:tag_ids]&.reject(&:blank?)
+
+    if item_ids.blank?
+      redirect_to items_path, alert: t('items.bulk_assign_tags.no_items_selected')
+      return
+    end
+
+    if tag_ids.blank?
+      redirect_to items_path, alert: t('items.bulk_assign_tags.no_tags_selected') 
+      return
+    end
+
+    # Find items and authorize each
+    items = Item.where(id: item_ids)
+    items.each { |item| authorize item, :edit? }
+    
+    tags = Tag.where(id: tag_ids)
+    
+    assigned_count = 0
+    items.each do |item|
+      tags.each do |tag|
+        # Only create association if it doesn't already exist
+        unless item.tagables.exists?(tag: tag)
+          item.tagables.create(tag: tag)
+          assigned_count += 1
+        end
+      end
+    end
+
+    if assigned_count > 0
+      redirect_to items_path, notice: t('items.bulk_assign_tags.success', count: assigned_count, items: items.count, tags: tags.count)
+    else
+      redirect_to items_path, notice: t('items.bulk_assign_tags.already_assigned')
+    end
+  rescue Pundit::NotAuthorizedError
+    redirect_to items_path, alert: t('items.bulk_assign_tags.unauthorized')
+  end
+
   private
 
   def set_item
