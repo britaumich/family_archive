2a51d4aa55326ace11f18c453b2ec30bef10f681
Margarita Barvinok <brita@umich.edu>
2026-02-06 23:34:24 -0500

    do not use the tagret - delete it

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 import GLightbox from "glightbox"
 
 export default class extends Controller {
-  static targets = ["gallery"]
   
   connect() {
     this.initializeLightbox()
