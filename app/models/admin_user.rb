47a7505f62f9d6612e671729cd17a4071f48b8e1
Margarita Barvinok <brita@umich.edu>
2026-02-14 18:06:39 -0500

    add validation

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -16,12 +16,13 @@ class AdminUser < ApplicationRecord
   normalizes :email, with: ->(e) { e.strip.downcase }
   
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :role, presence: true
 
   private
 
   def one_admin_user_should_exist
     if AdminUser.count == 1
-      errors.add(:base, "At least one admin user must exist.")
+      errors.add(:base, I18n.t('admin_user.errors.one_admin_user_must_exist'))
       throw(:abort)
     end
   end
