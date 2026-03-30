ab62eef666b0f75d6fc55a5d1a4f1273e5354fe0
Margarita Barvinok <brita@umich.edu>
2026-03-29 23:07:04 -0400

    refactor migration

--- db/migrate/20260330022031_add_translations_to_tags.rb
+++ db/migrate/20260330022031_add_translations_to_tags.rb
@@ -7,7 +7,13 @@ class AddTranslationsToTags < ActiveRecord::Migration[8.1]
       dir.up do
         Tag.reset_column_information
         Tag.find_each do |tag|
-          tag.update_column(:name_translations, { 'en' => tag.name })
+          migration_tag = Class.new(ActiveRecord::Base) do
+            self.table_name = 'tags'
+          end
+          migration_tag.reset_column_information
+          migration_tag.find_each do |tag|
+            tag.update_column(:name_translations, { 'en' => tag.name })
+          end
         end
       end
     end
