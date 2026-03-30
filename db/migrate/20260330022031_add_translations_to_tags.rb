0430fb920aadbff99c83a2d55793b828fc9a9ef9
Margarita Barvinok <brita@umich.edu>
2026-03-29 23:21:20 -0400

    edit migration again

--- db/migrate/20260330022031_add_translations_to_tags.rb
+++ db/migrate/20260330022031_add_translations_to_tags.rb
@@ -5,15 +5,12 @@ class AddTranslationsToTags < ActiveRecord::Migration[8.1]
     # Migrate existing names to English
     reversible do |dir|
       dir.up do
-        Tag.reset_column_information
-        Tag.find_each do |tag|
-          migration_tag = Class.new(ActiveRecord::Base) do
-            self.table_name = 'tags'
-          end
-          migration_tag.reset_column_information
-          migration_tag.find_each do |tag|
-            tag.update_column(:name_translations, { 'en' => tag.name })
-          end
+        migration_tag = Class.new(ActiveRecord::Base) do
+          self.table_name = 'tags'
+        end
+        migration_tag.reset_column_information
+        migration_tag.find_each do |tag|
+          tag.update_column(:name_translations, { 'en' => tag.name })
         end
       end
     end
