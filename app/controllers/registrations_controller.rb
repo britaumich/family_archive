d6b939ccfff1bedd63fddc341762e1a6426e3d56
Margarita Barvinok <brita@umich.edu>
2026-08-26 09:15:26 -0400

    registration for invitation only

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,9 +7,20 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    unless admin_user_for_email(@user.email_address)
+      @user.errors.add(:email_address, t('auth.registration_not_allowed'))
+      render :new, status: :unprocessable_entity
+      return
+    end
+
     if @user.save
-      start_new_session_for(@user)
-      redirect_to root_url, notice: t('forms.flash.registered_successfully')
+      if start_new_session_for(@user)
+        redirect_to root_url, notice: t('forms.flash.registered_successfully')
+      else
+        @user.errors.add(:email_address, t('auth.login_not_allowed'))
+        render :new, status: :unprocessable_entity
+      end
     else
       render :new, status: :unprocessable_entity
     end
